91608918909546fcd48fb2eb44068460bf4e9a99
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2025-07-05 19:30:30 +0530

    fix: add url to every navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Navbar from './Navbar'
 import { LuBus } from "react-icons/lu";
+import { useNavigate } from 'react-router-dom';
 
 
 const Header = () => {
+
+  const navigate = useNavigate()
+
   return (
     <div className='flex justify-between border border-transparent sticky top-0 shadow px-4 md:px-16 py-4 bg-white'>
-        <div className='flex cursor-pointer'>
+        <div className='flex cursor-pointer' onClick={() =>navigate('/') }>
             <LuBus style={{color: 'blue' , fontSize:'30px' , marginTop:'4'}}/>
             <h1 className='font-bold text-2xl md:text-3xl btn-secondary mx-1 md:mx-2'>TripTix</h1>
         </div>
