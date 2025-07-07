b2040c411391b3b4a620d2201f6e9ca9e9892fdb
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2025-07-08 00:10:09 +0530

    feat: add context provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,39 @@
 import React from 'react'
 import './App.css'
-import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Header from './components/Header'
 import Home from './pages/Home'
+import Footer from './components/Footer'
+import AvailableBus from './pages/AvailableBus'
+import SeatSelection from './components/SeatSelection'
+import PassengerDetails from './pages/PassengerDetails'
+import TrackBus from './components/TrackBus'
+import Login from './components/Login'
+import Profile from './pages/Profile'
+import BusProvider from './context/BusProvider'
 
 function App() {
 
   return (
-    <Router>
-      <Header/>
-      <div>
-        <Routes>
-           <Route path='/' element={<Home/>}/>
-        </Routes>
-      </div>
-    </Router>
+    <BusProvider>
+        <Router>
+        <Header/>
+        <div>
+          <Routes>
+            <Route path='/' element={<Home/>}/>
+            <Route path='/available-seat' element={<AvailableBus/>}/>
+            <Route path='/seat-selection/:id' element={<SeatSelection/>}/>
+            <Route path='/passenger-details' element={<PassengerDetails/>}/>
+            <Route path='/track-bus' element={<TrackBus/>}/>
+            <Route path='/login' element={<Login/>}/>
+            <Route path='/profile' element={<Profile/>}/>
+
+
+          </Routes>
+        </div>
+        <Footer/>
+        </Router>
+    </BusProvider>
   
   )
 }
