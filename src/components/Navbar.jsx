cea4a56215d825f0d2bc374398d14a38d25758b0
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2025-07-07 23:59:53 +0530

    feat: enhance navbar with search icon, routing and theme selection

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,28 +2,47 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { LuBus, LuUser } from "react-icons/lu";
 import { BsBrightnessHigh } from "react-icons/bs";
+import { IoIosSearch } from "react-icons/io";
+import { token } from '../utils/icons/constant';
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
 
 
 const Navbar = () => {
   return (
     <div className='flex '>
         <div className='flex  rounded-md px-4 py-2 text-blue-700 font-semibold bg-blue-200 cursor-pointer'>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-4 mt-1">
-            <path fillRule="evenodd" d="M10.5 3.75a6.75 6.75 0 1 0 0 13.5 6.75 6.75 0 0 0 0-13.5ZM2.25 10.5a8.25 8.25 0 1 1 14.59 5.28l4.69 4.69a.75.75 0 1 1-1.06 1.06l-4.69-4.69A8.25 8.25 0 0 1 2.25 10.5Z" clipRule="evenodd" />
-            </svg>
-            {/* <searchIcons/> */}
-            <Link className='mx-1 text-md'>Search</Link>
+            <IoIosSearch style={{fontSize:'20px' , marginTop:'3px'}}/>
+            <Link className='mx-1 text-md' to="/">Search</Link>
         </div>
         <div className='flex  rounded-md px-4 py-2 hover:text-blue-700 mx-4 text-gray-500'>
             <LuBus style={{marginTop:'4', marginLeft:'4'}}/>
-            <Link className='mx-1 text-md'>Track Bus</Link>
+            <Link className='mx-1 text-md' to="track-bus">Track Bus</Link>
         </div>
          <div className='flex border border-gray-200 hover:bg-blue-100 rounded-md px-4 py-2 mx-4 cursor-pointer'>
             <LuUser style={{marginTop:'4', marginLeft:'4'}}/>
-            <Link className='mx-1 text-md font-semibold'>Sign In</Link>
+            {token ? (<Link className='mx-1 text-md font-semibold' to="/profile"> Profile</Link>) : (<Link className='mx-1 text-md font-semibold' to="/login"> Sign In</Link>) }
         </div>
         <div className='flex border border-gray-200 hover:bg-blue-100 rounded-md px-3 py-2 mx-4 cursor-pointer'>
             <BsBrightnessHigh style={{marginTop:'4', marginLeft:''}}/>
+
+            <Select>
+  <SelectTrigger className="w-[180px]">
+    <SelectValue placeholder="Theme" />
+  </SelectTrigger>
+  <SelectContent>
+    <SelectItem value="light">Light</SelectItem>
+    <SelectItem value="dark">Dark</SelectItem>
+    <SelectItem value="system">System</SelectItem>
+  </SelectContent>
+</Select><command />
         </div>
     </div>
   )
