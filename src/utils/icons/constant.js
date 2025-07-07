c6a27d605c3699034b71c8624347649552d13036
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2025-07-08 00:15:29 +0530

    feat: add contant file

---
+++ src/utils/icons/constant.js
@@ -0,0 +1,6 @@
+export const Register_API = "http://localhost:3000/api/v1/user/signup"
+export const Login_Api = "http://localhost:3000/api/v1/login"
+export const Profile_API="http://localhost:3000/api/v1/user"
+export const Route_API="http://localhost:3000/api/v1/route?"
+export const Bus_API="http://localhost:3000/api/v1/bus?"
+export const token = localStorage.getItem('authorization')
