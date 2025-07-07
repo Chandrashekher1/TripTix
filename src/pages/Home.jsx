bef09c6c17aaac7e4d8a7c82b03925c13dd2ce0b
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2025-07-07 23:42:12 +0530

    feat: enhance home page with busCard, offerCard components

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import SearchBar from '../components/SearchBar'
+import BusCard from '../components/BusCard'
+import OfferCard from '../components/OfferCard'
+import WhyChoose from '../components/WhyChoose'
 
 const Home = () => {
   return (
@@ -9,6 +12,24 @@ const Home = () => {
             <p className='my-4 text-xl text-gray-600'>Book Comfortable bus tickets across the country with real-time seat selection</p>
         </div>
         <SearchBar/>
+
+        <div className='my-12'>
+          <h1 className='font-bold text-3xl text-center'>Top Travel Buses</h1>
+          <p className='my-4 text-center text-gray-700'>Premium comfort meets unbeatable prices</p>
+          <BusCard/>
+        </div>
+
+        <div className=''>
+          <h1 className='text-center font-bold text-3xl'>Special Offers</h1>
+          <p className='text-center text-gray-700 my-4'>Don't mess out on these amazing deals</p>
+
+          <OfferCard/>
+        </div>
+
+        <div className='bg-gradient-to-l from-blue-50 to-white my-8'>
+          <h1 className='font-bold text-3xl text-center my-8 '>Why choose TripTix</h1>
+          <WhyChoose/>
+        </div>
     </div>
   )
 }
