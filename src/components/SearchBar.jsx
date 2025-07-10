59e8bb82a34f7c560a0fc1d3762113d4c948c565
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2025-07-10 23:20:19 +0530

    fix: enhance layout and enhance responsiveness

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -40,55 +40,67 @@ const SearchBar = () => {
   };
 
   return (
-    <div className='mx-4'>
-      <form onSubmit={handleSearch} className='flex flex-col justify-center border px-4 py-8 shadow-xl bg-white rounded-md border-transparent'>
-        <label className='flex text-lg font-semibold text-gray-700 my-2'>
-          <IoLocationOutline style={{ marginTop: '4px', color: "blue", marginRight: '4px' }} /> From
-        </label>
-        <input
-          type="text"
-          value={origin}
-          onChange={(e) => setOrigin(e.target.value)}
-          placeholder='Departure city'
-          className='border px-4 py-3 rounded-md border-gray-400'
-          required
-        />
-
-        <div className='flex justify-center items-center my-4'>
-          <FaArrowRight style={{ color: 'blue' }} />
+    <div className='mx-4 bg-white shadow-xl border rounded-md border-transparent px-8 py-8 md:mx-40'>
+      <div>
+          <form onSubmit={handleSearch} className='flex flex-col  md:flex-row flex-wrap'>
+        <div>
+          <label className='flex text-lg font-semibold text-gray-700 my-2'>
+            <IoLocationOutline style={{ marginTop: '4px', color: "blue", marginRight: '4px' }} /> From
+          </label>
+          <input
+            type="text"
+            value={origin}
+            onChange={(e) => setOrigin(e.target.value)}
+            placeholder='Departure city'
+            className='border px-4 py-3 rounded-md border-gray-400'
+            required
+          />
         </div>
 
-        <label className='flex text-lg font-semibold text-gray-700 my-2'>
-          <IoLocationOutline style={{ marginTop: '4px', color: 'green', marginRight: '4px' }} /> To
-        </label>
-        <input
-          type="text"
-          value={destination}
-          onChange={(e) => setDestination(e.target.value)}
-          placeholder='Destination city'
-          className='border px-4 py-3 rounded-md border-gray-400'
-          required
-        />
+        <div className='flex justify-center items-center my-4 md:mx-4'>
+          <FaArrowRight style={{ color: 'blue' , marginTop:'20px' }} />
+        </div>
 
-        <label className='flex text-gray-700 font-semibold my-2'>
-          <MdOutlineCalendarToday style={{ marginTop: '4px', marginRight: '2px' }} /> Journey Date
-        </label>
-        <input
-          type="date"
-          value={date}
-          name='select date'
-          min={new Date().toISOString().split("T")[0]}
-          placeholder='Select date'
-          onChange={(e) => setDate(e.target.value)}
-          className='border px-4 py-3 rounded-md border-gray-400'
-          required
-        />
+        <div className='md:mx-16'>
+          <label className='flex text-lg font-semibold text-gray-700 my-2'>
+            <IoLocationOutline style={{ marginTop: '4px', color: 'green', marginRight: '4px' }} /> To
+          </label>
+          <input
+            type="text"
+            value={destination}
+            onChange={(e) => setDestination(e.target.value)}
+            placeholder='Destination city'
+            className='border px-4 py-3 rounded-md border-gray-400'
+            required
+          />
+        </div>
 
-        <button className='btn-primary flex font-semibold my-8 justify-center'>
+        <div>
+          <label className='flex text-gray-700 font-semibold my-2'>
+            <MdOutlineCalendarToday style={{ marginTop: '4px', marginRight: '2px' }} /> Journey Date
+          </label>
+          <input
+            type="date"
+            value={date}
+            name='select date'
+            min={new Date().toISOString().split("T")[0]}
+            placeholder='Select date'
+            onChange={(e) => setDate(e.target.value)}
+            className='border px-4 md:px-12 py-3 rounded-md border-gray-400'
+            required
+          />
+        </div>
+
+        <button className='btn-primary flex font-semibold my-8 justify-center md:mx-8 md:my-10 cursor-pointer hover:bg-blue-700' type='submit'>
           <CiSearch style={{ fontSize: '20px', marginRight: '8px' }} /> Search Buses
         </button>
 
-        <h3 className='font-semibold text-gray-600'>Popular Routes</h3>
+        
+      </form>
+      </div>
+      <div>
+            <div>
+              <h3 className='font-semibold text-gray-600'>Popular Routes</h3>
         <div className='flex flex-wrap gap-2'>
           <button
             type="button"
@@ -96,12 +108,13 @@ const SearchBar = () => {
               setOrigin('Delhi');
               setDestination('Dehradun');
             }}
-            className='flex border rounded-full px-4 py-1 bg-blue-100 text-blue-700 font-semibold'
+            className='flex border border-transparent rounded-full px-4 py-1 bg-blue-100 text-blue-700 font-semibold md:my-2'
           >
             Delhi <IoIosArrowRoundForward style={{ marginTop: '4px', marginLeft: '4px', marginRight: '4px' }} /> Dehradun
           </button>
+          </div>
         </div>
-      </form>
+      </div>
     </div>
   );
 };
