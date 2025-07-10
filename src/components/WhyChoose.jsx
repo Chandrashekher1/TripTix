3c37bb9ba945bab6a0d4665b37e290d2c901062f
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2025-07-10 23:22:22 +0530

    refactor: enhance layout and improved responsiveness

--- src/components/WhyChoose.jsx
+++ src/components/WhyChoose.jsx
@@ -4,18 +4,18 @@ import { MdOutlineWatchLater } from "react-icons/md";
 import { FaRegStar } from "react-icons/fa";
 const WhyChoose = () => {
   return (
-    <div className='mx-4'>
-        <div className='flex flex-col justify-center items-center'>
+    <div className='mx-4 md:flex flex-wrap md:mx-16 justify-center items-center my-8 md:h-80'>
+        <div className='flex flex-col justify-center items-center md:mx-4 md:w-80'>
             <span className='rounded-full p-4 bg-blue-100'><MdOutlineSecurity style={{color:'blue', fontSize:'40px'}}/></span>
             <h2 className='text-center font-semibold text-xl my-4'>Safe & Secure</h2>
-            <p className='text-center text-gray-700'>Your safety is our priority with verified operators and secure payments</p>
+            <p className='text-center text-gray-700 '>Your safety is our priority with verified operators and secure payments</p>
         </div>
-       <div className='flex flex-col justify-center items-center my-4'>
+       <div className='flex flex-col justify-center items-center my-4 md:w-80'>
             <span className='rounded-full p-4 bg-green-100'><MdOutlineWatchLater style={{color:'green', fontSize:'40px'}}/></span>
             <h2 className='text-center font-semibold text-xl my-4'>On-Time Service</h2>
             <p className='text-center text-gray-700'>Real-time tracking ensures you never miss your bus</p>
         </div>
-        <div className='flex flex-col justify-center items-center my-4'>
+        <div className='flex flex-col justify-center items-center my-4 md:w-80'>
             <span className='rounded-full p-4 bg-orange-100'><FaRegStar style={{color:'orange', fontSize:'40px'}}/></span>
             <h2 className='text-center font-semibold text-xl my-4'>Best Prices</h2>
             <p className='text-center text-gray-700'>Compare  prices and get best deals on every route</p>
