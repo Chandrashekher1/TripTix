f4a52ebc0159e9ceec4675c4f3598b5b98793e8d
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2025-07-16 09:45:11 +0530

    fix: correct Bus_API endpoint

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -2,5 +2,6 @@ export const Register_API = "http://localhost:3000/api/v1/user/signup"
 export const Login_Api = "http://localhost:3000/api/v1/login"
 export const Profile_API="http://localhost:3000/api/v1/user"
 export const Route_API="http://localhost:3000/api/v1/route?"
-export const Bus_API="http://localhost:3000/api/v1/bus?"
+export const Bus_API="http://localhost:3000/api/v1/bus"
+export const Seat_API="http://localhost:3000/api/v1/seat"
 export const token = localStorage.getItem('authorization')
