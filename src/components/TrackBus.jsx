1d2ac47c2596d9ec816c3c719f490ad85dc174f3
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2025-07-21 00:01:34 +0530

    feat: add responsiven layout and changes design

--- src/components/TrackBus.jsx
+++ src/components/TrackBus.jsx
@@ -3,15 +3,15 @@ import { CiSearch } from "react-icons/ci";
 
 const TrackBus = () => {
   return (
-    <div className='home py-8'>
-        <h1 className='text-center font-bold text-3xl'>Track Your Bus</h1>
-        <p className='text-center text-gray-700 my-2'>Enter your booking ID or bus number to track real-time location</p>
+    <div className='py-8 border-b border-b-gray-800'>
+        <h1 className='text-center font-bold text-3xl text-white'>Track Your Bus</h1>
+        <p className='text-center text-gray-400 my-2'>Enter your booking ID or bus number to track real-time location</p>
 
         <div className='md:mx-60 mx-4'>
             <div>
-            <form action="" className='flex border bg-white py-6 px-4 my-4 rounded-md border-gray-100 shadow-xl w-full' onSubmit={(e) => e.preventDefault()}>
-                <input type="text" placeholder='Enter booking ID or bus number' className='p-2 rounded-md border border-gray-200 w-full'/>
-                <button className='btn-primary flex font-semibold mx-4 '><CiSearch style={{marginTop:'4px', marginRight:'4px', fontSize:'20px'}}/> Track</button>
+            <form action="" className='flex border border-gray-900 bg-[#060e23] py-6 px-4 my-4 rounded-lg shadow-xl w-full' onSubmit={(e) => e.preventDefault()}>
+                <input type="text" placeholder='Enter booking ID or bus number' className='p-2 rounded-lg border border-gray-800 text-gray-400 focus:outline-blue-600 w-full'/>
+                <button className='btn-primary flex font-semibold mx-4 cursor-pointer'><CiSearch style={{marginTop:'4px', marginRight:'4px', fontSize:'20px'}}/> Track</button>
             </form>
         </div>
         <div className='border border-blue-200 rounded-md bg-blue-50 p-6'>
