0329950d24ebf6bd8b53d2f2421ec246da5f5b8c
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2025-07-21 00:03:21 +0530

    style: adjust extra pading and margin

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,28 +2,27 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { LuBus, LuUser } from "react-icons/lu";
 import { BsBrightnessHigh } from "react-icons/bs";
+import { FaRegMoon } from "react-icons/fa";
 import { IoIosSearch } from "react-icons/io";
 import { token } from '../utils/constant';
 
 
 const Navbar = () => {
   return (
-    <div className='flex '>
-        <div className='flex  rounded-md px-4 py-2 text-blue-700 font-semibold bg-blue-200 cursor-pointer'>
+    <div className='flex dark'>
+        <div className='flex  rounded-md px-4 py-2 text-blue-700 font-semibold btn-primary cursor-pointer'>
             <IoIosSearch style={{fontSize:'20px' , marginTop:'3px'}}/>
             <Link className='mx-1 text-md' to="/">Search</Link>
         </div>
-        <div className='flex  rounded-md px-4 py-2 hover:text-blue-700 mx-4 text-gray-500'>
+        <div className='flex  rounded-md px-4 py-2 hover:text-blue-700 mx-4 text-white'>
             <LuBus style={{marginTop:'4', marginLeft:'4'}}/>
-            <Link className='mx-1 text-md' to="track-bus">Track Bus</Link>
+            <Link className='mx-1 ' to="track-bus">Track Bus</Link>
         </div>
-         <div className='flex border border-gray-200 hover:bg-blue-100 rounded-md px-4 py-2 mx-4 cursor-pointer'>
+         <div className='flex borde rounded-md px-4 py-2 cursor-pointer text-white hover:text-blue-700'>
             <LuUser style={{marginTop:'4', marginLeft:'4'}}/>
             {token ? (<Link className='mx-1 text-md font-semibold' to="/profile"> Profile</Link>) : (<Link className='mx-1 text-md font-semibold' to="/login"> Sign In</Link>) }
         </div>
-        <div className='flex border border-gray-200 hover:bg-blue-100 rounded-md px-3 py-2 mx-4 cursor-pointer'>
-            <BsBrightnessHigh style={{marginTop:'4', marginLeft:''}}/>
-        </div>
+        
     </div>
   )
 }
