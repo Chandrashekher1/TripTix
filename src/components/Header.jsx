71ccdf0b7887adcf10b2b90d1d868950c220d25d
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2025-07-21 00:05:35 +0530

    style: adjust extra padding

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
   const navigate = useNavigate()
 
   return (
-    <div className='flex justify-between border border-transparent sticky top-0 shadow px-4 md:px-16 py-4 bg-white'>
+    <div className='flex justify-between border-b border-b-gray-700 sticky top-0 shadow px-4 md:px-16 py-4 bg-gray-950 '>
         <div className='flex cursor-pointer' onClick={() =>navigate('/') }>
             <LuBus style={{color: 'blue' , fontSize:'30px' , marginTop:'4'}}/>
             <h1 className='font-bold text-2xl md:text-3xl btn-secondary mx-1 md:mx-2'>TripTix</h1>
