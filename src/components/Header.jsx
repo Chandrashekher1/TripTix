22b93d43f1aa72b3f189fbb885055a8433836ebc
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2025-07-24 23:23:36 +0530

    style: adjust margin and padding

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,16 +5,15 @@ import { useNavigate } from 'react-router-dom';
 
 
 const Header = () => {
-
   const navigate = useNavigate()
 
   return (
-    <div className='flex justify-between border-b border-b-gray-700 sticky top-0 shadow px-4 md:px-16 py-4 bg-gray-950 '>
+    <div className='flex justify-between border-b border-b-gray-700 sticky top-0 shadow px-4 md:px-16 py-4 bg-gray-950 z-10'>
         <div className='flex cursor-pointer' onClick={() =>navigate('/') }>
             <LuBus style={{color: 'blue' , fontSize:'30px' , marginTop:'4'}}/>
             <h1 className='font-bold text-2xl md:text-3xl btn-secondary mx-1 md:mx-2'>TripTix</h1>
         </div>
-        <div className='hidden md:inline-block'>
+        <div className=''>
             <Navbar/>
         </div>
     </div>
