e4f9f17a952a5da1256005ad13d7ff8fc792b992
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2025-07-25 22:57:04 +0530

    feat: add bus gif

---
+++ src/components/OpenPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const OpenPage = () => {
+  return (
+    <div className='flex justify-center bg-[#f5fefa] items-center min-h-screen'>
+        <img src="src/assets/icons8-bus.gif" alt="" className='w-50 h-50 rounded-2xl p-4'/>
+    </div>
+  )
+}
+
+export default OpenPage
