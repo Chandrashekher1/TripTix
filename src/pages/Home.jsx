9cb2c4c451d169da6db26407deff3666ca95fc67
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2025-07-25 15:41:53 +0530

    feat: add testinomial component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import SearchBar from '../components/SearchBar'
 import BusCard from '../components/BusCard'
-import OfferCard from '../components/OfferCard'
 import WhyChoose from '../components/WhyChoose'
+import Feedback from '../components/Feedback'
 
 const Home = () => {
   return (
@@ -17,17 +17,22 @@ const Home = () => {
         </div>
         <SearchBar/>
 
-        <div className='bg-[#050b1c] py-4 my-8'>
+        <div className='bg-white py-4 my-8'>
           <div className='md:w-[60vw] md:mx-auto mx-4 flex flex-col items-center justify-center my-auto'>
-               <h1 className='font-bold text-3xl md:text-4xl text-center my-4 text-white '>Why choose TripTix?</h1>
-                <p className='text-gray-400 text-center text-xl'>Experience the future of bus travel with our cutting-edge features designed to make your journey smooth and worry-free.</p>
+               <h1 className='font-bold text-3xl md:text-4xl text-center my-4'>Why choose <span className='text-[#23a983]'>TripTix?</span></h1>
+                <p className='text-gray-500 text-center text-xl'>Experience the future of bus travel with our cutting-edge features designed to make your journey smooth and worry-free.</p>
           </div>
           <WhyChoose/>
         </div>
-        <div className='border-b border-b-gray-800 py-12'>
+        <div className='py-12'>
             <h1 className='font-bold text-3xl text-center text-white'>Top Travel Buses</h1>
             <p className='my-4 text-center text-gray-400 text-xl'>Discover our most popular routes with premium buses, great amenities, and unbeatable prices.</p>
             <BusCard/>
+        </div>
+        <div className='bg-[#f5fefa] py-24'>
+            <h1 className='font-semibold text-4xl text-center '>What Our <span className='text-[#23a983] font-bold'>Riders</span> Say</h1>
+
+            <Feedback/>
         </div>
     </div>
   )
