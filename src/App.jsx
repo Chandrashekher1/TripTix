08d44b80b0023b18ed3b6c43610078197e5cc543
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2025-07-25 22:56:25 +0530

    feat: add opening icons paeg

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Header from './components/Header'
@@ -8,25 +8,33 @@ import AvailableBus from './pages/AvailableBus'
 import SeatSelection from './components/SeatSelection'
 import PassengerDetails from './pages/PassengerDetails'
 import TrackBus from './components/TrackBus'
-import Login from './components/Login'
 import Profile from './pages/Profile'
 import BusProvider from './context/BusProvider'
 import ChatBot from './components/ChatBot'
+import OpenPage from './components/OpenPage'
 
 function App() {
+    const [loading, setLoading] = useState(true);
 
-  return (
+    useEffect(() => {
+      const timer = setTimeout(() => setLoading(false), 2000);
+      return () => clearTimeout(timer);
+    }, []);
+
+    if (loading) return <OpenPage />;
+
+
+    return (
     <BusProvider>
         <Router>
         <Header/>
-        <div className='bg-gray-950'>
+        <div>
           <Routes>
             <Route path='/' element={<Home/>}/>
             <Route path='/available-seat' element={<AvailableBus/>}/>
             <Route path='/seat-selection/:id' element={<SeatSelection/>}/>
             <Route path='/passenger-details' element={<PassengerDetails/>}/>
             <Route path='/track-bus' element={<TrackBus/>}/>
-            <Route path='/login' element={<Login/>}/>
             <Route path='/profile' element={<Profile/>}/>
           </Routes>
         </div>
