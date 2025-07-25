1810fe44fa4a694daeede38d542d10629bc2c2c7
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2025-07-25 15:07:58 +0530

    style: change header bg and logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,10 +8,9 @@ const Header = () => {
   const navigate = useNavigate()
 
   return (
-    <div className='flex justify-between border-b border-b-gray-700 sticky top-0 shadow px-4 md:px-16 py-4 bg-gray-950 z-10'>
+    <div className='flex justify-between border-b border-b-gray-700 sticky top-0 shadow-lg px-4 md:px-16 py-4 bg-white  z-10'>
         <div className='flex cursor-pointer' onClick={() =>navigate('/') }>
-            <LuBus style={{color: 'blue' , fontSize:'30px' , marginTop:'4'}}/>
-            <h1 className='font-bold text-2xl md:text-3xl btn-secondary mx-1 md:mx-2'>TripTix</h1>
+            <h1 className='font-bold text-2xl md:text-2xl  mx-1 md:mx-2 text-black'> <span className='border rounded-lg text-white bg-[#23a983] px-2'>T</span> TripTix</h1>
         </div>
         <div className=''>
             <Navbar/>
