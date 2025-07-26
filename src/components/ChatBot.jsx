0c9d6db21c10a250094c9d12528ba5ac94e99faf
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2025-07-26 16:09:46 +0530

    refactor: change ui of chatbot

--- src/components/ChatBot.jsx
+++ src/components/ChatBot.jsx
@@ -15,8 +15,6 @@ const ChatBot = () => {
   const chatContainerRef = useRef();
 
   const ai = new GoogleGenAI({ apiKey: import.meta.env.VITE_GPT_API_KEY });
-
-  // Auto-scroll to bottom when new message arrives
   useEffect(() => {
     if (chatContainerRef.current) {
       chatContainerRef.current.scrollTop = chatContainerRef.current.scrollHeight;
@@ -73,17 +71,17 @@ const ChatBot = () => {
   return (
     <div className="fixed bottom-4 right-4 z-50">
       {isOpen ? (
-        <div className="w-[340px] h-[460px]  rounded-2xl shadow-2xl flex flex-col border border-gray-900">
-          <div className="btn-primary text-white p-4 flex justify-between items-center rounded-t-2xl">
+        <div className="w-[340px] h-[460px]  rounded-2xl shadow-2xl flex flex-col border border-transparent">
+          <div className="bg-[#39b290] text-white p-4 flex justify-between items-center rounded-t-2xl">
             <h3 className="font-bold text-lg">TripTix Assistant</h3>
             <IoClose className="cursor-pointer text-2xl" onClick={() => setIsOpen(false)} />
           </div>
 
-          <div ref={chatContainerRef} className="flex-1 overflow-y-auto px-4 py-3 text-sm text-gray-700 bg-[#060e23]">
+          <div ref={chatContainerRef} className="flex-1 overflow-y-auto px-4 py-3 text-sm text-gray-700 bg-white">
             {message.map((msg, index) => (
               <div key={index} className={`flex mb-2 ${msg.type === 'user' ? 'justify-end' : 'justify-start'}`}>
                 <div className={`rounded-2xl px-4 py-2 text-sm max-w-[80%] shadow-md flex items-start gap-2 ${
-                  msg.type === 'user' ? 'bg-blue-600 text-white rounded-br-none' : 'bg-gray-900 text-gray-300 rounded-bl-none'
+                  msg.type === 'user' ? 'bg-[#39b290] text-white rounded-br-none' : 'bg-gray-100 text-gray-700 rounded-bl-none'
                 }`}>
                   {msg.type === 'user' ? <FiUser className="mt-0.5" /> : <RiRobot2Line className="text-xl mt-0.5 text-blue-600" />}
                   <span>{msg.text}</span>
@@ -92,34 +90,34 @@ const ChatBot = () => {
             ))}
             {loading && (
               <div className="flex justify-start text-gray-500 text-xs italic mt-1">
-                <span>Typing...</span>
+                <span>Thinking...</span>
               </div>
             )}
           </div>
 
-          <div className="flex flex-wrap p-2 gap-2 border-t border-t-gray-700 bg-[#060e23]">
+          <div className="flex flex-wrap p-2 gap-2 border-t border-t-gray-300 bg-white">
             {["How to book bus?", "How to cancel ticket?", "Refund Policies", "Contact support"].map((text, idx) => (
               <button
                 key={idx}
                 onClick={() => handleSuggestedClick(text)}
-                className="px-3 py-1 text-sm bg-gray-900 text-blue-700 rounded-full hover:bg-gray-700 cursor-pointer transition"
+                className="px-3 py-1 text-sm bg-white text-gray-800 rounded-full hover:bg-green-100 hover:text-green-900 font-semibold border border-gray-100 shadow-md cursor-pointer transition"
               >
                 {text}
               </button>
             ))}
           </div>
 
-          <div className="p-2 border-t bg-gray-800 flex gap-2">
+          <div className="p-2 border-t bg-gray-800 flex gap-2 bg-white">
             <input
               ref={query}
               type="text"
               placeholder="Type your question..."
-              className="w-full p-2 border text-gray-300 rounded-md text-sm "
+              className="w-full p-2 border border-gray-300 text-gray-700 rounded-md text-sm bg-white focus:outline-green-300 focus:ring-2"
               onKeyDown={(e) => e.key === 'Enter' && handleAsk()}
             />
             <button
               onClick={handleAsk}
-              className="btn-primary text-white px-4 py-2 rounded-md text-sm hover:bg-blue-700 transition cursor-pointer"
+              className="bg-[#23a983] text-white px-4 py-2 rounded-md text-sm hover:bg-[#23a983] transition cursor-pointer"
             >
               Ask
             </button>
@@ -127,7 +125,7 @@ const ChatBot = () => {
         </div>
       ) : (
         <button
-          className="bg-blue-600 text-white p-4 rounded-full shadow-xl hover:bg-blue-700 transition cursor-pointer"
+          className="bg-[#39b290] text-white p-4 rounded-full shadow-xl hover:bg-[#23a983] transition cursor-pointer"
           onClick={() => setIsOpen(true)}
         >
           <IoChatbubbleEllipsesOutline className="text-2xl" />
