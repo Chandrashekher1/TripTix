16e2e0fd9f67e7e75f8da1ef80a4fc16be611f6d
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2025-07-26 21:41:51 +0530

    feat: add motion

--- src/pages/PassengerDetails.jsx
+++ src/pages/PassengerDetails.jsx
@@ -5,6 +5,7 @@ import { useLocation, useNavigate } from 'react-router-dom';
 import { Booking_API, Bus_API, Razorpay_API } from '../utils/constant';
 import { io } from 'socket.io-client';
 import AuthContext from '../context/AuthContext';
+import {motion} from "framer-motion"
 
 const PassengerDetails = () => {
   const navigate = useNavigate()
@@ -133,113 +134,127 @@ const PassengerDetails = () => {
     );
   };
 
-  
+  const fadeInUp = {
+    hidden: { opacity: 0, y: 60 },
+    visible: { opacity: 1, y: 0, transition: { duration: 0.6, ease: "easeOut" } },
+  };
   return (
-    <div className='bg-gray-950 py-8 px-4 border-b border-b-gray-800'>
+    
+    <div className='bg-[#f5fefa] py-8 px-4 border-b border-b-gray-800'>
       <div className='flex justify-between md:justify-normal md:mx-40'>
-        <p className='flex font-semibold cursor-pointer text-white hover:border rounded-lg border-transparent px-4 hover:bg-gray-900' onClick={() => navigate('/available-seat')}>
-          <IoIosArrowRoundBack style={{ marginRight: '8px', fontSize: '30px' }} /> Back to Buses
+        <p className='flex font-semibold cursor-pointer text-black hover:border rounded-lg border-transparent px-4 hover:bg-green-200' onClick={() => navigate('/available-seat')}>
+          <IoIosArrowRoundBack className='my-1 mx-2 text-xl'/> Back to Buses
         </p>
-        <h1 className='flex flex-col font-bold text-2xl md:mx-16 text-white'>
-          Passenger Details <span className='text-lg font-medium text-gray-400'>Fill details for all passengers</span>
+        <h1 className='flex flex-col font-bold text-2xl md:mx-16 text-black'>
+          Passenger Details <span className='text-lg font-medium text-gray-700'>Fill details for all passengers</span>
         </h1>
       </div>
+      <motion.div
+      variants={fadeInUp}
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true }}
+      >
 
-      <div className='md:mx-40 md:flex md:gap-8'>
-        <div className='flex flex-col gap-6 md:w-2/3 my-2'>
-        {state?.selectedSeats?.map((seat, index) => (
-          <form key={seat} className="bg-gray-900 rounded-xl p-6 text-white shadow-md">
-            <p className="font-semibold text-lg mb-4 flex items-center gap-2 text-white">
-              <LuUser className="text-xl text-blue-400" />
-              Passenger {index + 1} 
-            </p>
+        <div className='md:mx-40 md:flex md:gap-8'>
+          <div className='flex flex-col gap-6 md:w-2/3 my-2'>
+          {state?.selectedSeats?.map((seat, index) => (
+            <form key={seat} className="bg-white rounded-xl p-6 text-black shadow-lg">
+              <p className="font-semibold text-lg mb-4 flex items-center gap-2 text-black">
+                <LuUser className="text-xl text-black" />
+                Passenger {index + 1} 
+              </p>
 
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-              <div>
-                <label className="block text-sm font-semibold mb-1">Full Name *</label>
-                <input
-                  type="text"
-                  required
-                  placeholder="Enter full name"
-                  value={passengerDetails[index].name}
-                  onChange={(e) => {
-                    const updated = [...passengerDetails];
-                    updated[index].name = e.target.value;
-                    setPassengerDetails(updated);
-                  }}
-                  className="w-full px-4 py-2 rounded-md bg-gray-900 border border-gray-700 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500"
-                />
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                <div>
+                  <label className="block text-sm font-semibold mb-1">Full Name *</label>
+                  <input
+                    type="text"
+                    required
+                    placeholder="Enter full name"
+                    value={passengerDetails[index].name}
+                    onChange={(e) => {
+                      const updated = [...passengerDetails];
+                      updated[index].name = e.target.value;
+                      setPassengerDetails(updated);
+                    }}
+                    className="w-full px-4 py-2 rounded-md  border border-gray-700 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-green-500"
+                  />
+                </div>
+
+                <div>
+                  <label className="block text-sm font-semibold mb-1">Age *</label>
+                  <input
+                    type="number"
+                    required
+                    placeholder="Enter age"
+                    value={passengerDetails[index].age}
+                    onChange={(e) => {
+                      const updated = [...passengerDetails];
+                      updated[index].age = e.target.value;
+                      setPassengerDetails(updated);
+                    }}
+                    className="w-full px-4 py-2 rounded-md  border border-gray-700 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-green-500"
+                  />
+                </div>
+
+                <div>
+                  <label className="block text-sm font-semibold mb-1">Gender *</label>
+                  <select
+                    required
+                    value={passengerDetails[index].gender}
+                    onChange={(e) => {
+                      const updated = [...passengerDetails];
+                      updated[index].gender = e.target.value;
+                      setPassengerDetails(updated);
+                    }}
+                    className="w-full px-4 py-2 rounded-md  border border-gray-700 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-green-500"
+                  >
+                    <option value="" disabled>Select gender</option>
+                    <option value="Male">Male</option>
+                    <option value="Female">Female</option>
+                    <option value="Other">Other</option>
+                  </select>
+                </div>
+                
               </div>
+            </form>
 
-              <div>
-                <label className="block text-sm font-semibold mb-1">Age *</label>
-                <input
-                  type="number"
-                  required
-                  placeholder="Enter age"
-                  value={passengerDetails[index].age}
-                  onChange={(e) => {
-                    const updated = [...passengerDetails];
-                    updated[index].age = e.target.value;
-                    setPassengerDetails(updated);
-                  }}
-                  className="w-full px-4 py-2 rounded-md bg-gray-900 border border-gray-700 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500"
-                />
-              </div>
+          ))}
 
-              <div>
-                <label className="block text-sm font-semibold mb-1">Gender *</label>
-                <select
-                  required
-                  value={passengerDetails[index].gender}
-                  onChange={(e) => {
-                    const updated = [...passengerDetails];
-                    updated[index].gender = e.target.value;
-                    setPassengerDetails(updated);
-                  }}
-                  className="w-full px-4 py-2 rounded-md bg-gray-900 border border-gray-700 text-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
-                >
-                  <option value="" disabled>Select gender</option>
-                  <option value="Male">Male</option>
-                  <option value="Female">Female</option>
-                  <option value="Other">Other</option>
-                </select>
-              </div>
-              
-            </div>
-          </form>
-
-        ))}
-
-        </div>
-
-        <div className='bg-gray-900 text-white rounded-md p-6 shadow mt-8 md:mt-0 md:w-1/3 h-fit sticky top-24'>
-          <h1 className='font-semibold text-2xl mb-4'>Booking Summary</h1>
-          <p className='text-lg font-semibold'>{`${BusDetails.operator || ''} - ${BusDetails.busType || ''}`}</p>
-
-          <p className='flex justify-between mt-2'>Departure <span className='font-bold'>{departureTime}</span></p>
-          <p className='flex justify-between'>Arrival <span className='font-bold'>{arrivalTime}</span></p>
-          <p className='flex justify-between'>Duration <span className='font-bold'>{getDuration(departureTime,arrivalTime)}</span></p>
-
-          <div className='border-b my-4 border-gray-200'></div>
-          <div className='border-b border-gray-300 my-4'></div>
-          <div>
-            <p className='flex justify-between my-2'>Base Fare ({state.selectedSeats.length}) <span>₹{BusDetails.price * state.selectedSeats.length}</span></p>
-            <p className='flex justify-between my-2 font-bold text-xl'>Total <span className='text-blue-700'>₹{BusDetails.price * state.selectedSeats.length}</span></p>
           </div>
 
-            <button
-            className={`btn-primary w-full mt-4 cursor-pointer ${
-              !isFormValid() ? 'opacity-50 cursor-not-allowed' : ''
-            }`}
-            onClick={handlePayment}
-            disabled={!isFormValid()}
-            >
-            Proceed to Payment
-          </button>
+          <div className='bg-white rounded-lg p-6 shadow-lg mt-8 md:mt-0 md:w-1/3 h-fit sticky top-24'>
+            <h1 className='font-semibold text-2xl mb-4'>Booking Summary</h1>
+            <p className='text-lg font-semibold'>{`${BusDetails.operator || ''} - ${BusDetails.busType || ''}`}</p>
+
+            <p className='flex justify-between mt-2'>Departure <span className='font-bold'>{departureTime}</span></p>
+            <p className='flex justify-between'>Arrival <span className='font-bold'>{arrivalTime}</span></p>
+            <p className='flex justify-between'>Duration <span className='font-bold'>{getDuration(departureTime,arrivalTime)}</span></p>
+
+            <div className='border-b my-4 border-gray-200'></div>
+            <div className='border-b border-gray-300 my-4'></div>
+            <div>
+              <p className='flex justify-between my-2'>Base Fare ({state.selectedSeats.length}) <span>₹{BusDetails.price * state.selectedSeats.length}</span></p>
+              <p className='flex justify-between my-2 font-bold text-xl'>Total <span className='text-blue-700'>₹{BusDetails.price * state.selectedSeats.length}</span></p>
+            </div>
+
+              <button
+              className={`btn-primary w-full mt-4 cursor-pointer ${
+                !isFormValid() ? 'opacity-50 cursor-not-allowed' : ''
+              }`}
+              onClick={handlePayment}
+              disabled={!isFormValid()}
+              >
+              Proceed to Payment
+            </button>
+          </div>
         </div>
-      </div>
+      </motion.div>
     </div>
+
+
+   
   );
 }
 
